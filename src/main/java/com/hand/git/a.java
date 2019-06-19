8533b6373ac23537da1ce4c05378f1a7ed22db29
tom_glb <jianguo.chen_glb@hand-china.com>
2019-06-19 14:12:06 +0800

    2

--- src/main/java/com/hand/git/a.java
+++ src/main/java/com/hand/git/a.java
@@ -2,7 +2,7 @@ package com.hand.git;
 
 public class a {
 	public static void main(String[] args) {
-		System.out.print(gg);
+		System.out.print(aaa);
 		System.out.print(123);
 		System.out.print(eee);
 		System.out.print(4);
