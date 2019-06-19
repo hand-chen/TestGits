95f10c5917f8848581faabb5684b617cc03c194f
hand-chen <49428898+hand-chen@users.noreply.github.com>
2019-06-19 13:36:56 +0800

    Update a.java

--- src/main/java/com/hand/git/a.java
+++ src/main/java/com/hand/git/a.java
@@ -2,7 +2,7 @@ package com.hand.git;
 
 public class a {
 	public static void main(String[] args) {
-		System.out.print(0);
+		System.out.print(gg);
 		System.out.print(123);
 		System.out.print(1);
 		System.out.print(4);
