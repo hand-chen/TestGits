5d4c795da71cf1018c611ddf2f2d700c9f7f53a5
tom_glb <jianguo.chen_glb@hand-china.com>
2019-06-19 11:38:09 +0800

    test

--- src/main/java/com/hand/git/a.java
+++ src/main/java/com/hand/git/a.java
@@ -4,7 +4,7 @@ public class a {
 	public static void main(String[] args) {
 		System.out.print(0);
 		System.out.print(1111);
-		System.out.print(2);
+		System.out.print(1);
 		System.out.print(4);
 	}
 
